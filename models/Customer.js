e39e462b3f19628c8bc72eb4fe23c73815a53e74
MUHAMMED ADEEB VENGATTUPARAMBIL <139254311+MuhammedAdeeb@users.noreply.github.com>
2026-03-29 12:37:27 +0530

    Delete models directory

--- models/Customer.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const CustomerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  phone: { type: String, required: true },
-  address: { type: String, required: true },
-}, { timestamps: true });
-
-export default mongoose.models.Customer || mongoose.model('Customer', CustomerSchema);
